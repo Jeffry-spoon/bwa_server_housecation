4140ef2542d19ae47630202158ab5a92d46bf04c
Jeffry <jeffryacai@gmail.com>
2021-11-20 21:16:01 +0700

    input picture

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,6 +1,30 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+// import uuid from "uuid/v4";
 
+// const storageMultiple = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     var dir = 'public/images';
+//     if (!fs.existsSync(dir)) {
+//       fs.mkdirSync(dir);
+//     }
+//     cb(null, dir);
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   }
+// })
+
+// const uploadMultiple = multer({
+//   storage: storageMultiple,
+//   limits: { fileSize: 1000000 },
+//   fileFilter: function (req, file, cb) {
+//     checkFileType(file, cb);
+//   }
+// }).array("image", 12);
+
+// Set storage engine
 const storage = multer.diskStorage({
   destination: "public/images",
   filename: function (req, file, cb) {
@@ -10,11 +34,11 @@ const storage = multer.diskStorage({
 
 const uploadSingle = multer({
   storage: storage,
-  // limits: { fileSize: 1000000 },
+  limits: { fileSize: 1000000 },
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   },
-}).single("images");
+}).single("image");
 
 const uploadMultiple = multer({
   storage: storage,
@@ -24,14 +48,20 @@ const uploadMultiple = multer({
   },
 }).array("images");
 
+// // Check file Type
 function checkFileType(file, cb) {
+  // Allowed ext
   const fileTypes = /jpeg|jpg|png|gif/;
+  // Check ext
   const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
   const mimeType = fileTypes.test(file.mimetype);
+
   if (mimeType && extName) {
     return cb(null, true);
   } else {
     cb("Error: Images Only !!!");
   }
 }
-module.exports = { uploadSingle, uploadMultiple };
+
+module.exports = {  uploadSingle , uploadMultiple,};
